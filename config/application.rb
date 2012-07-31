a277f77f966ee1bd0ad07015c216b9ace3965932
NatashaMurashev <nmurashev@gmail.com>
2012-07-31 08:40:02 -0700

    no longer compresses assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module Pollster
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.compress = false
   end
 end
