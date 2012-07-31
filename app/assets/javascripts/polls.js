bd3d9248edf4916f3140275adf34059cc79bf0b3
NatashaMurashev <nmurashev@gmail.com>
2012-07-31 12:47:50 -0700

    form questions now submit correctly with params

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -1,18 +1,19 @@
 $(function() {
 	var numFields = 1
 	
-	function addField(){
-		$(question = '#question' + numFields).unbind('keypress');
-		numFields ++;
-		var id = 'question' + numFields;
-		$('<br /><input type="text" id=' + id + '>').insertAfter('#question' + (numFields - 1));
-		appendHandlerToFields();
-	};
-	
 	function appendHandlerToFields(){
 		$('#question' + numFields).keypress(function() {
 				addField();
 		});
+	};
+	
+	function addField(){
+		$(question = '#question' + numFields).unbind('keypress');
+		numFields ++;
+		var id = 'question' + numFields;
+		var param = id + '[text]'
+		$('<br /><input type="text" id=' + id + ' name=' + id +'>').insertAfter('#question' + (numFields - 1));
+		appendHandlerToFields();
 	};
 	
 	appendHandlerToFields();
