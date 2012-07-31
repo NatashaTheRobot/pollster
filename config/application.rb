4839e88bf6d8790197fc49c23c29b59f341bd167
NatashaMurashev <nmurashev@gmail.com>
2012-07-31 08:47:12 -0700

    updated the uglifier gem outside of assests group

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,5 @@ module Pollster
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.compress = false
   end
 end
